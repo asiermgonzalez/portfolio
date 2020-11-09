73dbdd1afc9bd2b73ddf48fc31d354200a0f98e8
Asier <asiermgonzalez@outlook.es>
2020-11-09 19:12:51 +0100

    portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-    HeaderComponent
-  ],
+    HeaderComponent,
+    PortfolioComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
